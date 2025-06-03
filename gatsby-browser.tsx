66d4903eb809f815455cc329935b54142f2f8b32
himanshu <himanshu.chhabra@razorpay.com>
2025-06-03 13:45:49 +0530

    fix: vscode styles

--- gatsby-browser.tsx
+++ gatsby-browser.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { ThemeProvider } from './src/hooks/themeContext';
 import './src/styles/global.scss';
+import 'gatsby-remark-vscode/styles.css';
 
 export const wrapRootElement = ({ element }) => {
   return <ThemeProvider>{element}</ThemeProvider>;
