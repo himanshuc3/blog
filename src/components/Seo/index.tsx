ee525b1468085753239bb415b79a912f8ab51cdf
himanshu <himanshu.chhabra@razorpay.com>
2025-06-10 19:07:44 +0530

    fix: localstorage and favicon

--- src/components/Seo/index.tsx
+++ src/components/Seo/index.tsx
@@ -79,10 +79,6 @@ export const SEO = ({ title, description, pathname, children, keywords }: ISEOMe
         },
       ]}
     >
-      <link
-        rel="icon"
-        href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='0.9em' font-size='90'>👤</text></svg>"
-      />
       {children}
     </Helmet>
   );
--- src/hooks/themeContext.tsx
+++ src/hooks/themeContext.tsx
@@ -1,4 +1,4 @@
-import React, { useState, createContext, ReactNode } from 'react';
+import React, { useState, createContext, ReactNode, useEffect } from 'react';
 
 interface ContextProps {
   darkTheme: boolean;
@@ -16,11 +16,23 @@ interface Props {
 }
 
 export const ThemeProvider: React.FC<Props> = ({ children }) => {
-  const [darkTheme, setDarkTheme] = useState(false);
+  const [darkTheme, setDarkTheme] = useState(true);
+
+  useEffect(() => {
+    const theme = localStorage.getItem('theme');
+    const isDarkTheme = theme ? theme === 'dark' : true;
+    setDarkTheme(isDarkTheme);
+    if (isDarkTheme) {
+      document.documentElement.classList.add('dark');
+    }
+  }, []);
 
   const toggleThemeHandler = () => {
     document.documentElement.classList.toggle('dark');
     setDarkTheme((prevState) => !prevState);
+    setTimeout(() => {
+      localStorage.setItem('theme', darkTheme ? 'light' : 'dark');
+    }, 0);
   };
 
   return (
